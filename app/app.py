a6812d170c2ca1c7adb787b3e5f2786643a0ec29
qianxunchen <2511284740@qq.com>
2020-03-30 00:13:45 +0800

    改调试地址

--- app/app.py
+++ app/app.py
@@ -14,7 +14,7 @@ app.register_blueprint(V)
 
 
 if __name__=='__main__':
-  # app.run(debug=True)
-  app.run(host='0.0.0.0', debug=True)
+  app.run(debug=True)
+  # app.run(host='0.0.0.0', debug=True)
 
 
